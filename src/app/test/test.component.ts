8010d994716672c2e7e527dda42eb64e7f1b13cc
erikwiklander <erik.wiklander@wiklandia.com>
2018-10-19 23:08:32 +0200

    Better

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,28 +1,36 @@
+import { Comment } from './test.model';
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
-import { AngularFirestore } from 'angularfire2/firestore';
-import { defineBase } from '@angular/core/src/render3';
+import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
 
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
   styleUrls: ['./test.component.css']
 })
+
 export class TestComponent implements OnInit {
 
-  public items: Observable<any[]>;
+  public itemCollection: AngularFirestoreCollection<Comment>;
   public newComment: string;
+  public comments: Comment[] = [];
 
   constructor(private db: AngularFirestore) {
-    this.items = db.collection('/items').valueChanges();
+    this.itemCollection = db.collection<Comment>('/items');
+    this.itemCollection.ref.orderBy('creationDate', 'desc').onSnapshot(
+      querySnapshot => {
+        this.comments = [];
+        querySnapshot.forEach(
+          queryDocumentSnapshot => this.comments.push(queryDocumentSnapshot.data() as Comment));
+      });
    }
 
   ngOnInit() {
   }
 
   onEnter() {
-    this.db.collection('items').add(
-      {comment: this.newComment, date: new Date()});
+    const comment = new Comment(this.newComment);
+    this.itemCollection.add({...comment});
     this.newComment = '';
   }
 
---
+++ src/app/test/test.model.ts
@@ -0,0 +1,8 @@
+
+
+export class Comment {
+    creationDate: Date;
+    constructor(public comment: string) {
+      this.creationDate = new Date();
+    }
+  }
