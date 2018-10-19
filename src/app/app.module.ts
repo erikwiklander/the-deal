595b3c844e0e3e746371bfaa97d2f4bcfa333cb7
erikwiklander <erik.wiklander@wiklandia.com>
2018-10-19 13:50:19 +0200

    Adding new things to db

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatCheckboxModule, MatIconModule, MatToolbarModule } from '@angular/material';
+import { MatButtonModule, MatCheckboxModule, MatIconModule, MatToolbarModule, MatInputModule } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { AppRoutingModule } from './app-routing.module';
@@ -16,6 +16,7 @@ import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { AngularFireModule } from 'angularfire2';
 
 import { environment } from '../environments/environment';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -34,6 +35,8 @@ import { environment } from '../environments/environment';
     MatToolbarModule,
     MatSidenavModule,
     AppRoutingModule,
+    MatInputModule,
+    FormsModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule
   ],
--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AngularFirestore } from 'angularfire2/firestore';
+import { defineBase } from '@angular/core/src/render3';
 
 @Component({
   selector: 'app-test',
@@ -10,12 +11,19 @@ import { AngularFirestore } from 'angularfire2/firestore';
 export class TestComponent implements OnInit {
 
   public items: Observable<any[]>;
+  public newComment: string;
 
-  constructor(db: AngularFirestore) {
+  constructor(private db: AngularFirestore) {
     this.items = db.collection('/items').valueChanges();
    }
 
   ngOnInit() {
+  }
+
+  onEnter() {
+    this.db.collection('items').add(
+      {comment: this.newComment, date: new Date()});
+    this.newComment = '';
   }
 
 }
